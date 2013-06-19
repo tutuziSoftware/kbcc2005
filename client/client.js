c51d2d0862516c0aded7c6896b39a679830b4a6e
yuki_kago <puruhime@gmail.com>
2013-06-20 00:14:58 +0900

    調整。一応音楽に合うようになった

--- client/client.js
+++ client/client.js
@@ -1,47 +1,52 @@
 function test($scope, $timeout){
-	$scope.timelines = [
-		new kbcc2005.Timeline([
-			new kbcc2005.Pop(3),
-			new kbcc2005.Pop(10),
-			new kbcc2005.Pop(18),
-			new kbcc2005.Pop(25)
-		]),
-		new kbcc2005.Timeline([
-			new kbcc2005.Pop(4),
-			new kbcc2005.Pop(11),
-			new kbcc2005.Pop(19),
-			new kbcc2005.Pop(26)
-		]),
-		new kbcc2005.Timeline([
-			new kbcc2005.Pop(6),
-			new kbcc2005.Pop(14),
-			new kbcc2005.Pop(21),
-			new kbcc2005.Pop(29)
-		]),
-		new kbcc2005.Timeline([
-			new kbcc2005.Pop(7),
-			new kbcc2005.Pop(15),
-			new kbcc2005.Pop(22),
-			new kbcc2005.Pop(30)
-		])
+	$scope.pops = [
+		{
+			key:'Q',
+			timeline:new kbcc2005.Timeline([
+				new kbcc2005.Pop(3),
+				new kbcc2005.Pop(10),
+				new kbcc2005.Pop(18),
+				new kbcc2005.Pop(25)
+			])
+		},
+		{
+			key:'W',
+			timeline:new kbcc2005.Timeline([
+				new kbcc2005.Pop(4),
+				new kbcc2005.Pop(11),
+				new kbcc2005.Pop(19),
+				new kbcc2005.Pop(26)
+			])
+		},
+		{
+			key:'E',
+			timeline:new kbcc2005.Timeline([
+				new kbcc2005.Pop(7),
+				new kbcc2005.Pop(14),
+				new kbcc2005.Pop(21),
+				new kbcc2005.Pop(29)
+			])
+		},
+		{
+			key:'R',
+			timeline:new kbcc2005.Timeline([
+				new kbcc2005.Pop(8),
+				new kbcc2005.Pop(15),
+				new kbcc2005.Pop(22),
+				new kbcc2005.Pop(30)
+			])
+		}
 	];
 	$scope.good = 0;
 	$scope.bad = 0;
 
-	//Q, W, E, R
-	var keyCodes = [81, 87, 69, 82];
-	$scope.keys = keyCodes.map(function(keyCode){
-		return String.fromCharCode(keyCode);
-	});
-
 	$("body").keydown(function(e){
-		for(var i = 0 ; i != keyCodes.length ; i++){
-			if(keyCodes[i] === e.keyCode){
-				console.log(keyCodes[i]);
-				$scope.timelines[i].fire();
+		for(var i = 0 ; i != $scope.pops.length ; i++){
+			if($scope.pops[i].key.charCodeAt(0) === e.keyCode){
+				$scope.pops[i].timeline.fire();
 
-				$scope.good = $scope.timelines.reduce(count.bind(null, "good"), 0);
-				$scope.bad = $scope.timelines.reduce(count.bind(null, "bad"), 0);
+				$scope.good = $scope.pops.reduce(count.bind(null, "good"), 0);
+				$scope.bad = $scope.pops.reduce(count.bind(null, "bad"), 0);
 
 				$scope.$apply();
 				return;
@@ -52,10 +57,10 @@ function test($scope, $timeout){
 	$scope.start = function(){
 		//タイムラインを動かします
 		$timeout(function(){
-			if($scope.timelines.every(checkTimelineEndAll)) return;
+			if($scope.pops.every(checkTimelineEndAll)) return;
 
-			$scope.timelines.forEach(function(timeline){
-				timeline.movePops();
+			$scope.pops.forEach(function(timeline){
+				timeline.timeline.movePops();
 			});
 
 			$timeout(arguments.callee, 1000);
@@ -65,12 +70,12 @@ function test($scope, $timeout){
 
 
 	//good、badを集計します
-	function count(key, count, timeline){
-		count += timeline[key];
+	function count(key, count, pop){
+		count += pop.timeline[key];
 		return count;
 	}
 
-	function checkTimelineEndAll(timeline){
-		return timeline.isEnd;
+	function checkTimelineEndAll(pop){
+		return pop.timeline.isEnd;
 	}
 }
